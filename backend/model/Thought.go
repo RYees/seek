9e1f178aaf0e5ebf814f2efbc4dad5757dd140de
SamixDev <samer.hd.ah@gmail.com>
2023-02-27 15:36:30 +0200

    feat: index thoughts medias

--- backend/model/Thought.go
+++ backend/model/Thought.go
@@ -2,15 +2,18 @@ package model
 
 import (
 	"time"
+
+	"gorm.io/datatypes"
 )
 
 type Thought struct {
-	ID           uint64    `gorm:"primaryKey" json:"id"`
-	Creator      string    `json:"creator"`
-	Header       string    `json:"header"`
-	Message      string    `json:"message"`
-	CreationDate time.Time `json:"creation_date"`
-	BlockHeight  uint64    `json:"block_height"`
+	ID           uint64         `gorm:"primaryKey" json:"id"`
+	Creator      string         `json:"creator"`
+	Header       string         `json:"header"`
+	Message      string         `json:"message"`
+	Media        datatypes.JSON `json:"media"`
+	CreationDate time.Time      `json:"creation_date"`
+	BlockHeight  uint64         `json:"block_height"`
 
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
